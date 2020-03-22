b85695cb5ce156fba04a78a55d438c6aa0016c3a
Meguri, Ofir (om804c) <om804c@intl.att.com>
2020-03-23 00:00:49 +0200

    remove unnecessary imports

--- src/tester.kt
+++ src/tester.kt
@@ -8,9 +8,7 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.nio.file.FileSystems
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
